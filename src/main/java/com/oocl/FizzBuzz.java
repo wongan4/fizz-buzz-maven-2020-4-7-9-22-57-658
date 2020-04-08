a050b32960415300f30ad0cce3a1e59fb70d78f3
Andy Wong <andy.wong@oocl.com>
2020-04-08 16:07:53 +0800

    [Andy] pass test case of normal number

---
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -0,0 +1,13 @@
+package com.oocl;
+
+public class FizzBuzz {
+    private String result;
+
+    public void say(int i) {
+        this.result = String.valueOf(i);
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+}
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -2,10 +2,15 @@ package com.oocl;
 
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     @Test
-    public void helloWorld() {
+    public void should_result_be_number_when_say_normal_number() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
 
+        fizzBuzz.say(1);
+
+        assertEquals(fizzBuzz.getResult(), "1");
     }
 }
