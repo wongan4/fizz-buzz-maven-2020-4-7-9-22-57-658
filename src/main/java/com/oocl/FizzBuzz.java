abe71ecf0daab321135682d44db5ae59968da699
Andy Wong <andy.wong@oocl.com>
2020-04-08 16:15:02 +0800

    [Andy] pass test case of saying multiple of 3

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -4,7 +4,11 @@ public class FizzBuzz {
     private String result;
 
     public void say(int i) {
-        this.result = String.valueOf(i);
+        if (i % 3 == 0) {
+            this.result = "fizz";
+        } else {
+            this.result = String.valueOf(i);
+        }
     }
 
     public String getResult() {
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -1,16 +1,27 @@
 package com.oocl;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void should_result_be_number_when_say_normal_number() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         fizzBuzz.say(1);
-
         assertEquals(fizzBuzz.getResult(), "1");
+    }
+
+    @Test
+    public void should_result_be_fizz_when_say_multiple_of_three() {
+        fizzBuzz.say(3);
+        assertEquals(fizzBuzz.getResult(), "fizz");
     }
 }
